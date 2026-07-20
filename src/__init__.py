86846ce4c3b826f375a109fbb8dff90e3c904d15
LUIS ALFONSO SALCEDO <ponchitosalcedo1315@gmail.com>
2026-07-19 19:49:06 -0600

    Delete src directory

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Módulo principal del sistema de monitorización
-"""
-__version__ = "1.0.0"
-__author__ = "Luis Alfonso Salcedo Peña"
--- src/core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Módulo core del sistema de monitorización
-"""
-from .monitoring_system import MonitoringSystem
-from .drift_detector import DriftDetector
-from .metrics_calculator import MetricsCalculator
-
-__all__ = ['MonitoringSystem', 'DriftDetector', 'MetricsCalculator']
